b56ab0ea051f93b096c8be56d49994bf9e8836e1
ARLDATADOJO <e.nuzzo@reply.com>
2024-08-31 14:15:34 +0100

    Add files via upload
    Prepared exercises for the first dojo

---
+++ exercises dojo (no solution) 1.sql
@@ -0,0 +1,58 @@
+
+------  select all customer name, surname, and date of birth  that were born after 01 March 1992
+
+ 
+
+
+--- count all lines in CustomerDetails table , name your statistic as "nlines"
+
+ 
+
+
+--- sum all the balance you have in  account details table 
+ 
+
+----  looking at the precendent question, sum the balance only for Saving accounts
+
+ 
+
+----  looking at the precendent question, sum the balance only for Checkings  accounts
+
+ 
+
+--- use a union clause, make a report that show the type of account and the sum of balance 
+--- for each of the two types, show as well the type of account as first column 
+
+ 
+
+
+--- obtain the same result of the precedent query WITHOUT union clause
+
+ 
+
+
+
+--- select all the different  surnames. According to you, is there (at least) a repeate surnname
+-- proof the second question using a DISTINCT
+-- ? 
+
+ 
+
+
+
+
+
+-- is it possible to retrieve the  surname that is duplicated? 
+-- Tip:  you can investigate all the the surname with a where or
+--- you can use some clauses we already used, as for the statistic nline 
+
+ 
+
+
+
+
+
+
+
+
+ 
